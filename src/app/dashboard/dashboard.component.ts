8dc363e968f8bef2fd315ee1dee6064cf0c4ffd1
Akash <akash1853@gmail.com>
2019-02-14 17:03:26 +0530

    Ngb modal added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+  	config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit() {
+  }
+
+  open(content) {
+    this.modalService.open(content);
   }
 
 }
