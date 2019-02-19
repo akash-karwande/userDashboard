81ed8bfee49d439a8120d51ca1ee10a78767c65d
Akash <akash1853@gmail.com>
2019-02-19 18:26:53 +0530

    final commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -12,23 +12,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DashboardComponent implements OnInit {
 	private modalRef;
 	dashboardForm:FormGroup;
-  public local:any;
+  public local = [
+                   {id:"1",protocol:"IP",sourceIP:"2.2.2.2",destinationIP:'3.3.5.5',accessType:'Permit'},
+                   {id:"2",protocol:"TCP",sourceIP:"2.2.3.3",destinationIP:'3.3.5.6',accessType:'Denied'},
+                   {id:"3",protocol:"HTTP",sourceIP:"2.2.4.4",destinationIP:'3.3.7.8',accessType:'All'},
+               ];
   constructor(config: NgbModalConfig, private modalService: NgbModal,private router:Router,private fb:FormBuilder) {}
 
   ngOnInit() {
-    this.local = localStorage.getItem('data');
+
+    localStorage.getItem('data');
+    localStorage.setItem('data', JSON.stringify(this.local));
     console.log(this.local);
 
   	this.dashboardForm = this.fb.group({
+            id: ['', Validators.compose([Validators.required])],
             protocol: ['', Validators.compose([Validators.required])],
             sourceIP: ['', Validators.compose([Validators.required, Validators.pattern('^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$')])],
             destinationIP: ['', Validators.compose([Validators.required, Validators.pattern('^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$')])],
-            accessType: ['', Validators.compose([Validators.required])],
-            id: ['', Validators.compose([Validators.required])]
+            accessType: ['', Validators.compose([Validators.required])]
+           
   });
-  	console.log(this.dashboardForm.value.destinationIP);
 
   }
+
 
   open(content) {
     this.modalRef = this.modalService.open(content);
--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -9,32 +9,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./rules.component.css']
 })
 export class RulesComponent implements OnInit {
-	dashboardForm:FormGroup;
-	public local:any;
-
+	dashboardForm2:FormGroup;
+   public local:any;
   constructor(private router:Router, private fb:FormBuilder) { }
 
   ngOnInit() {
-
-  	this.dashboardForm = this.fb.group({
+  	this.dashboardForm2 = this.fb.group({
             protocol: ['', Validators.compose([Validators.required])],
             sourceIP: ['', Validators.compose([Validators.required, Validators.pattern('^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$')])],
             destinationIP: ['', Validators.compose([Validators.required, Validators.pattern('^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$')])],
             accessType: ['', Validators.compose([Validators.required])],
             id: ['', Validators.compose([Validators.required])]
   });
-  }
-
-  goBack() {
-
-  	this.router.navigate(['dashboard']);
 
   }
 
    getForm() {
-  	this.local= localStorage.setItem('data',JSON.stringify(this.dashboardForm.value));
-  		console.log(this.local);
-  	
+     let a = JSON.stringify(this.dashboardForm2.value);
+     console.log(a);
+     this.local = localStorage.getItem('data');
+     console.log(this.local);
+     //this.local.push(a);
+     localStorage.setItem('data', this.local);
+     this.router.navigate(['dashboard']);
   }
 
 
